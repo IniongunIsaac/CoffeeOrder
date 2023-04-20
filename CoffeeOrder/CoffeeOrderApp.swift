bf0378589426e4f111b3ce68fa05570e419b3470
Isaac Iniongun <iniongunisaac@gmail.com>
2023-04-20 19:16:34 +0100

    Initial Commit

---
+++ CoffeeOrder/CoffeeOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeOrderApp.swift
+//  CoffeeOrder
+//
+//  Created by Isaac Iniongun on 20/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
